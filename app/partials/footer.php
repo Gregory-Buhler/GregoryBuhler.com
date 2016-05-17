b828f3626c2806f21bdfe005bb3f2f228c1e0346
Gregory-Buhler <webmaster@gregorybuhler.com>
2016-05-17 17:43:08 -0600

    Added linked in, and made reference links _blank for user experience

--- app/partials/footer.php
+++ app/partials/footer.php
@@ -1,8 +1,9 @@
       <footer>
         <div class="row">
           <div class="col-xs-12">
-            <a href="https://github.com/Gregory-Buhler/"><i class="fa fa-github-square" aria-hidden="true"></i></a>
-            <a href="http://codepen.io/Gregory-Buhler/"><i class="fa fa-codepen" aria-hidden="true"></i></a>
+            <a href="https://github.com/Gregory-Buhler/" target="_blank" rel="nofollow noreferrer"><i class="fa fa-github-square" aria-hidden="true"></i></a>
+            <a href="http://codepen.io/Gregory-Buhler/" target="_blank" rel="nofollow noreferrer"><i class="fa fa-codepen" aria-hidden="true"></i></a>
+            <a href="https://www.linkedin.com/in/gregorybuhler" target="_blank" rel="nofollow noreferrer"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
           </div>
         </div>
         <div class="row">
--- dist/partials/footer.php
+++ dist/partials/footer.php
@@ -1,16 +1,17 @@
-      <footer>
-        <div class="row">
-          <div class="col-xs-12">
-            <a href="https://github.com/Gregory-Buhler/"><i class="fa fa-github-square" aria-hidden="true"></i></a>
-            <a href="http://codepen.io/Gregory-Buhler/"><i class="fa fa-codepen" aria-hidden="true"></i></a>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12">
-            <p>Copyright Gregory Buhler &copy; 2016</p>
-          </div>
-        </div>
-      </footer>
+<footer>
+  <div class="row">
+    <div class="col-xs-12">
+      <a href="https://github.com/Gregory-Buhler/" target="_blank" rel="nofollow noreferrer"><i class="fa fa-github-square" aria-hidden="true"></i></a>
+      <a href="http://codepen.io/Gregory-Buhler/" target="_blank" rel="nofollow noreferrer"><i class="fa fa-codepen" aria-hidden="true"></i></a>
+      <a href="https://www.linkedin.com/in/gregorybuhler" target="_blank" rel="nofollow noreferrer"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
     </div>
-  </body>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+      <p>Copyright Gregory Buhler &copy; 2016</p>
+    </div>
+  </div>
+</footer>
+</div>
+</body>
 </html>
